a052dc96fa28b68d3258564318a81a3af9aa3a24
hitesh-dgk <hitesh.a.dingankar@gmail.com>
2024-03-19 23:23:48 +0530

    setup changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { Buffer } from 'buffer'
+import { Buffer } from 'buffer'
 
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@fontsource/mulish/latin-400.css'; // normal
 import '@fontsource/mulish/latin-500.css'; // medium
 import '@fontsource/mulish/latin-600.css'; // semibold
@@ -10,14 +10,15 @@ import '@fontsource/mulish/latin-700.css'; // bold
 import '@fontsource-variable/anybody/wdth.css';
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom"
-// import { WagmiProvider } from 'wagmi'
-// import { wagmiConfig } from './utils/wagmiConfig.ts';
+import { WagmiProvider } from 'wagmi'
+import { wagmiConfig } from './utils/wagmiConfig.ts';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 
-// const queryClient = new QueryClient()
-// globalThis.Buffer = Buffer
+const queryClient = new QueryClient()
+console.log("queryClient: ", queryClient)
+globalThis.Buffer = Buffer
 
 
 console.log("inside app component ")
@@ -25,16 +26,16 @@ console.log("inside app component ")
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-  // <WagmiProvider config={wagmiConfig}>
-    // <QueryClientProvider client={queryClient}>
+  <WagmiProvider config={wagmiConfig}>
+    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <BrowserRouter>
           {/* <PrivacyPassLayer> */}
-            <App />
+          <App />
           {/* </PrivacyPassLayer> */}
         </BrowserRouter>
       </Provider>
-    // </QueryClientProvider>
-  // </WagmiProvider>,
+    </QueryClientProvider>
+  </WagmiProvider>,
   // </React.StrictMode>,
 )
