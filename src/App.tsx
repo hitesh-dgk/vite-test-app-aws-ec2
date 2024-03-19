11a33fab298b2da615592f0d918c712c4332f722
hitesh-dgk <hitesh.a.dingankar@gmail.com>
2024-03-19 16:13:57 +0530

    minimize the app size

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { Suspense }   from "react";
-import { useAppSelector } from "./store/hooks";
-import { RootState } from "./store";
+// import { useAppSelector } from "./store/hooks";
+// import { RootState } from "./store";
 import { useWatchWagmiAccount } from "./utils/watchWagmiAccount";
 // import './index.scss'
 // import './App.scss'
 // import PrivacyPassLayer from "./modules/PrivacyPassLayer"
 // import MainApplicationHOC from "./components/MainApplicationHOC";
-const PrivacyPassLayer = React.lazy(() => import('./modules/PrivacyPassLayer'));
-const MainApplicationHOC = React.lazy(() => import("./components/MainApplicationHOC"));
+// const PrivacyPassLayer = React.lazy(() => import('./modules/PrivacyPassLayer'));
+// const MainApplicationHOC = React.lazy(() => import("./components/MainApplicationHOC"));
 
 
 
 function App() {
-  const { isConnected, isConnecting, isReconnecting, fetchingProtectedData, protectedData } = useAppSelector((state: RootState) => state.user);
+  // const { isConnected, isConnecting, isReconnecting, fetchingProtectedData, protectedData } = useAppSelector((state: RootState) => state.user);
 
   useWatchWagmiAccount();
   return (
--- src/components/InputText.tsx
+++
@@ -1,5 +0,0 @@
-import { twc } from 'react-twc';
-
-export const InputText = twc.input.attrs({
-  type: 'text',
-})`rounded-30 bg-grey-800 px-4 py-3`;
