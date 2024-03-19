9e426c019bc8b46d8d51f5f26fc63fca0d5d9c7f
hitesh-dgk <hitesh.a.dingankar@gmail.com>
2024-03-20 00:07:35 +0530

    consoles added

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+console.log("second line")
 import { Buffer } from 'buffer'
+console.log("buffer importted")
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+console.log("tanstack importted")
 import '@fontsource/mulish/latin-400.css'; // normal
 import '@fontsource/mulish/latin-500.css'; // medium
 import '@fontsource/mulish/latin-600.css'; // semibold
 import '@fontsource/mulish/latin-700.css'; // bold
 import '@fontsource-variable/anybody/wdth.css';
+console.log("all the fonts importted")
+
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom"
 import { WagmiProvider } from 'wagmi'
 import { wagmiConfig } from './utils/wagmiConfig.ts';
 import { Provider } from 'react-redux';
 import { store } from './store';
+console.log("upto store importted")
+
 
 
 const queryClient = new QueryClient()
