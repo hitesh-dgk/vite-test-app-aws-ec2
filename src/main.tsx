b7b92d5e4379f36770c0108d7d032576715f51e6
hitesh-dgk <hitesh.a.dingankar@gmail.com>
2024-03-19 14:49:30 +0530

    buffer import added

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
+import { Buffer } from 'buffer'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.tsx'
